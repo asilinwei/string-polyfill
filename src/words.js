9fff8773e54723216a382dd999eb86de23ba8123
asilinwei <1021069119@qq.com>
2018-10-06 19:34:41 +0800

    feat: add code about words.js
    Signed-off-by: asilinwei <1021069119@qq.com>

---
+++ src/words.js
@@ -0,0 +1,25 @@
+if (!String.prototype._words) {
+	String.prototype._words = (function() {
+		"use strict";
+		
+		var regTag = '[object RegExp]',
+			toString = Object.prototype.toString;
+
+		var isRegExp = function(o) {
+			return toString.apply(o) === regTag;
+		};
+
+		var other = function(string, pattern) {
+			var patternString = '' + pattern,
+				reg = new RegExp(patternString, 'g');
+
+			return string.match(reg);
+		};
+
+		return function(pattern) {
+			pattern = pattern !== undefined ? pattern : /[a-z]+/ig;
+
+			return isRegExp(pattern) ? this.match(pattern) : other(this, pattern);
+		};
+	})();
+}
